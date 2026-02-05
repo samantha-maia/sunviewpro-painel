3098d2e7888db986a7d57f70c2385c72717c975c
Renan Santos <renan.santos@doublex.com.br>
2026-02-05 12:58:51 -0300

    Fix missing import VarInfoUserStruct in cadastro_widget.dart

--- lib/flows/acesso_a_plataforma/cadastro/cadastro_widget.dart
+++ lib/flows/acesso_a_plataforma/cadastro/cadastro_widget.dart
@@ -1,4 +1,5 @@
 import '/backend/api_requests/api_calls.dart';
+import '/backend/schema/structs/index.dart';
 import '/components/modal_info/modal_info_widget.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
